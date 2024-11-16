3ad6633eee2410999071de595ef8e0ba1091d962
Litman <litman.ayala@softtek.com>
2024-11-15 20:57:52 -0500

    first commit

---
+++ MoviesChallengeViper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesChallengeViper
+//
+//  Created by Litman Ayala on 15/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MoviesChallengeViperTests/MoviesChallengeViperTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesChallengeViperTests.swift
+//  MoviesChallengeViperTests
+//
+//  Created by Litman Ayala on 15/11/24.
+//
+
+import Testing
+@testable import MoviesChallengeViper
+
+struct MoviesChallengeViperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
